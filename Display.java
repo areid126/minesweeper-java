1674869524bce55fcbdb0a64af246556441e65d0
Abbey Reid <ar383@st-andrews.ac.uk>
2025-07-26 15:14:58 +0100

    Created design for basic GUI components

---
+++ Display.java
@@ -0,0 +1,163 @@
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.Border;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.EtchedBorder;
+import javax.swing.border.MatteBorder;
+import javax.swing.border.SoftBevelBorder;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+
+public class Display {
+
+    public static void main(String[] args){
+        Display display = new Display();
+        display.setUpFrame();
+    }
+
+    // Create the frame to contain the board
+    private JFrame frame = new JFrame();
+    private Board board = new Board(8, 8, 10);
+    // JPanel panel = new JPanel(new GridLayout(board.getHeight(), board.getWidth()));
+    private ArrayList<ArrayList<GridButton>> buttons = new ArrayList<>();
+
+
+    // Method to set up the board
+    public void setUpFrame(){
+
+        // Create the two types of board
+        BevelBorder innerBorder = new BevelBorder(BevelBorder.LOWERED, Colours.unopenedTop, Colours.unopenedTop, Colours.unopenedBottom, Colours.unopenedBottom);
+        BevelBorder outerBorder = new BevelBorder(BevelBorder.RAISED, Colours.unopenedTop, Colours.unopenedTop, Colours.unopenedBottom, Colours.unopenedBottom);
+
+        // Create the main panel
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+        mainPanel.setBackground(Colours.unopened);
+        mainPanel.setBorder(BorderFactory.createCompoundBorder(outerBorder, new EmptyBorder(10, 10, 10, 10)));
+
+        // Create the header panel
+        JPanel headerPanel = new JPanel();
+        headerPanel.setLayout(new BoxLayout(headerPanel, BoxLayout.X_AXIS));
+        headerPanel.setBorder(BorderFactory.createCompoundBorder(innerBorder, new EmptyBorder(5, 5, 5, 5)));
+        headerPanel.setBackground(Colours.unopened);
+        mainPanel.add(headerPanel);
+
+        // Add the fields needed for the header
+        JLabel mineCount = new JLabel("099");
+        mineCount.setForeground(Colours.counter);
+        mineCount.setFont(new Font("Bitstream Vera Sans Mono", Font.BOLD, 20));
+        mineCount.setBackground(Color.BLACK);
+        mineCount.setOpaque(true);
+        mineCount.setBorder(innerBorder);
+
+        // Layout the restart button
+        JLabel restart = new JLabel();
+        // Layout the button
+        restart.setBorder(outerBorder);
+        restart.setBackground(Colours.unopened);
+        restart.setOpaque(true);
+        // Add the icon to the button
+        try {
+            BufferedImage buttonIcon = ImageIO.read(new File("img/smile.png"));
+            restart.setIcon(new ImageIcon(buttonIcon.getScaledInstance(25, -1, Image.SCALE_DEFAULT))); // Set the icon on the button
+            restart.setText("");
+        } catch (IOException e) {
+            restart.setText("S");
+        }
+
+        JLabel timer = new JLabel("000");
+        timer.setForeground(Colours.counter);
+        timer.setFont(new Font("Bitstream Vera Sans Mono", Font.BOLD, 20));
+        timer.setBackground(Color.BLACK);
+        timer.setOpaque(true);
+        timer.setBorder(innerBorder);
+
+        // Add the fields to the header panel
+        headerPanel.add(mineCount);
+        headerPanel.add(Box.createHorizontalGlue());
+        headerPanel.add(restart);
+        headerPanel.add(Box.createHorizontalGlue());
+        headerPanel.add(timer);
+
+        // Create the grid for the board
+        JPanel panel = new JPanel(new GridLayout(board.getHeight(), board.getWidth()));
+        for(int i = 0; i < board.getHeight(); i++){
+            buttons.add(new ArrayList<>());
+            for(int j = 0; j < board.getWidth(); j++){
+                
+                GridButton button = new GridButton(i, j, board.get(i, j));
+                button.addMouseListener(new MouseListener() {
+                    @Override
+                    public void mouseClicked(MouseEvent e){
+                    
+                    }
+        
+                    @Override
+                    public void mouseEntered(MouseEvent e){
+                       
+                    }
+        
+                    @Override
+                    public void mouseExited(MouseEvent e){
+                        
+                    }
+        
+                    @Override
+                    public void mousePressed(MouseEvent e){
+                        
+                    }
+        
+                    @Override
+                    public void mouseReleased(MouseEvent e){
+                        // If the left click is pressed
+                        if(e.getButton() == MouseEvent.BUTTON1){
+                            board.open(button.getRow(), button.getColumn());
+                            updateDisplay(); // Update the display after clicking a button
+                        } // If the right click is pressed
+                        if(e.getButton() == MouseEvent.BUTTON3){
+                            button.mark();
+                        }
+                        board.checkWon(); // Check if the game has been won after every click
+                    }
+                });
+                buttons.get(i).add(button); // add the buttons to the list of buttons
+                panel.add(button);
+            }
+        }
+
+        // Add the correct border to the panel
+        panel.setBorder(BorderFactory.createCompoundBorder(new EmptyBorder(10, 0, 0, 0), innerBorder));
+        panel.setBackground(Colours.unopened);
+        int size = 20, gridWidth = (board.getWidth()*size) - 3, gridHeight = (board.getHeight()*size) - 2;
+        panel.setPreferredSize(new Dimension(gridWidth, gridHeight));
+        panel.setMinimumSize(new Dimension(gridWidth, gridHeight));
+        panel.setMaximumSize(new Dimension(gridWidth, gridHeight));
+        mainPanel.add(panel);
+        frame.add(mainPanel);
+
+        // Format the frame
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setResizable(false);
+        frame.setVisible(true);
+    }
+
+    // Method to update the display
+    public void updateDisplay(){
+        for(int i = 0; i < board.getDisplay().size(); i++){
+            for(int j = 0; j < board.getDisplay().get(i).size(); j++){
+                if(board.getDisplay().get(i).get(j)){
+                    buttons.get(i).get(j).open();
+                }
+            }
+        }
+        frame.revalidate(); // Reload the display
+    }
+}
+
---
+++ GridButton.java
@@ -0,0 +1,142 @@
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.Border;
+import javax.swing.border.MatteBorder;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+public class GridButton extends JLabel{
+
+    // Store the buttons position in the grid and its value
+    private final int ROW;
+    private final int COLUMN;
+    private int value;
+    private boolean open = false;
+    private boolean marked = false;
+
+    public GridButton(int row, int column, int value){
+
+        this.ROW = row;
+        this.COLUMN = column;
+        this.value = value;
+
+        int size = 0;
+
+        // setMinimumSize(new Dimension(size, size));
+        setPreferredSize(new Dimension(size, size));
+        // setMaximumSize(new Dimension(size, size));
+
+
+        // Set basic layout things
+        setBorder(new BevelBorder(BevelBorder.RAISED, Colours.unopenedTop, Colours.unopenedBottom));
+        setBackground(Colours.unopened);
+        setOpaque(true);
+        setFont(new Font(Font.DIALOG, Font.BOLD, 15));
+        setHorizontalAlignment(SwingConstants.CENTER);
+        setVerticalAlignment(SwingConstants.CENTER);
+        // setBorderPainted(false);
+        // setFocusPainted(false);
+        setFocusable(false);
+        // setRolloverEnabled(false);
+        // setContentAreaFilled(false);
+    }
+
+    public int[] getPosition(){
+        return new int[]{ROW, COLUMN};
+    }
+
+    public int getValue(){
+        return value;
+    }
+
+    public int getRow(){
+        return ROW;
+    }
+
+    public int getColumn(){
+        return COLUMN;
+    }
+
+    public void mark(){
+        if(!open){
+            marked = !marked;
+            if(marked){
+                setButtonIcon("img/flag.png", "F");
+            }
+            else{
+                setButtonIcon(null, "");
+            }  
+        }
+    }
+
+    // Method to open a button after it has been pressed
+    public void open() {
+        if(!marked){
+            open = true;
+            setText(value + "");
+            setIcon(null); // Remove the icon when opening the cell
+            setBorder(new MatteBorder(1, 1, 0, 0, Colours.unopenedBottom));
+            // setRolloverEnabled(false);
+            switch(value){
+                case(0):
+                setText("");
+                break;
+                case(1):
+                setForeground(Colours.ONE);
+                break;
+                case(2):
+                setForeground(Colours.TWO);
+                break;
+                case(3):
+                setForeground(Colours.THREE);
+                break;
+                case(4):
+                setForeground(Colours.FOUR);
+                break;
+                case(5):
+                setForeground(Colours.FIVE);
+                break;
+                case(6):
+                setForeground(Colours.SIX);
+                break;
+                case(7):
+                setForeground(Colours.SEVEN);
+                break;
+                case(8):
+                setForeground(Colours.EIGHT);
+                break;
+                case(-1):
+                setButtonIcon("img/bomb.png", "B");
+                break;
+            }
+            
+        }
+    }
+
+    // Function for getting an icon
+    void setButtonIcon(String iconPath, String altText) {
+
+        // Set the icon if there is an icon to set
+        if(iconPath != null) {
+            try {
+                BufferedImage buttonIcon = ImageIO.read(new File(iconPath));
+                // setIcon(new ImageIcon(buttonIcon.getScaledInstance(40, -1, Image.SCALE_SMOOTH))); // Set the icon on the button
+                setIcon(new ImageIcon(buttonIcon)); // Set the icon on the button
+                setText("");
+            } catch (IOException e) {
+                // If there is an error loading the image then set the text to be the altText instead
+                setText(altText);
+            }
+        }
+        // Unset the icon if there is no icon to set
+        else {
+            setIcon(null);
+            setText("");
+        }
+
+    }
+}
